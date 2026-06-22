e1fb1d41dc7bbe3082824174af28f3ccc5ff177b
ArthurSantosAlmeida <arthursa.prof@gmail.com>
2026-06-22 16:32:36 -0300

    Set basename for BrowserRouter in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,7 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/front-end">
     <App />
   </BrowserRouter>,
 );
