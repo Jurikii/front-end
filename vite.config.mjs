2fac77e00cfda5f6af7238979bea45802d362d23
Arthur <arthursa.prof@gmail.com>
2026-06-22 17:17:37 -0300

    test github pages

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,6 +6,19 @@ export default defineConfig({
   build: {
     outDir: "build",
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "prefix-public-assets",
+      enforce: "post",
+      renderChunk(code) {
+        const base = "/front-end/";
+        const regex = /(["'(])\/([\w@.\-]+\.(?:png|svg|jpg|jpeg|gif|webp|ico|avif))(["')])/g;
+        const updated = code.replace(regex, `$1${base}$2$3`);
+        if (updated !== code) return { code: updated, map: null };
+        return null;
+      },
+    },
+  ],
   base: '/front-end/'
 });
